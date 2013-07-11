ebcc81bd24dc753d47be4bb98be9023f5f71d90c
Kevin-winbash <kevin.mccabe@locallighthouse.com>
2013-07-11 16:12:04 -0700

    moved php to its own file

--- hours.php
+++ hours.php
@@ -449,7 +449,7 @@ function get_default_hours(){
 </script>
 </head>
 <body>
-<form action="hours.php" method="post">
+<form action="datahandler.php" method="post">
     <table>
         <tr>
             <td><span class="link" id="repeat_all">repeat all</span></td><td><span id="open_column">repeat column</span></td><td style="width:25px; align: center;"></td><td><span id="close_column">repeat column</span></td>
@@ -458,46 +458,4 @@ function get_default_hours(){
     </table>
 </form>
 <span class="action twentyfourseven">24-7</span>   <span class="action closed_weekends">Closed Weekends</span>  <span class="action default_hours">Mon-Fri 9-5</span>
-
-<pre>
-<?php
-
-class Hours{
-    public $day_data;
-    public $days;
-    public $day_codes;
-    public function __construct($day_data){
-        $this->day_data = $day_data;
-        $this->days = array("monday"=>0, "tuesday"=>1, "wednesday"=>2, "thursday"=>3, "friday"=>4, "saturday"=>5, "sunday"=>6);
-        $this->day_codes = array("monday"=>"Mo", "tuesday"=>"Tu", "wednesday"=>"We", "thursday"=>"Th", "friday"=>"Fr", "saturday"=>"Sa", "sunday"=>"Su");
-    }
-    public function meta_hours(){
-        $i=0;
-        foreach($this->day_data as $key=>$value){
-            if($value['status']){
-                $timespan=$value['open']['hour'].$value['open']['minute'].$value['close']['hour'].$value['close']['minute'];
-                $groups[$timespan][]=$this->day_data[$key];
-            }
-            $i++;
-        }
-        return $groups;
-    }
-}
-$days['monday'] = $_POST['monday'];
-$days['tuesday'] = $_POST['tuesday'];
-$days['wednesday'] = $_POST['monday'];
-$days['thursday'] = $_POST['thursday'];
-$days['friday'] = $_POST['friday'];
-$days['saturday'] = $_POST['saturday'];
-$days['sunday'] = $_POST['sunday'];
-//foreach($days as $key=>$value){
-//    if($value['status']){
-//        $times[opens][]=$value['open']['hour'].':'.$value['open']['minute'];
-//        $times[closes][]=$value['close']['hour'].':'.$value['close']['minute'];
-//    }//
-//}
-$hours = new Hours($days);
-print_r($hours->meta_hours());
-?>
-</pre>
 </body>
